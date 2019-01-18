581d56b45b2443822a3600cab5be6a63ccb47547
Robin Bron <robin@finwo.nl>
2019-01-18 18:13:31 +0100

    Hook structure should work like this

--- yenta.js
+++ yenta.js
@@ -20,6 +20,53 @@
   // Fetch a reference to global
   let universe = new Function('return this').call();
 
+  // Our main constructor
+  function Yenta(options) {
+
+    // Sanity checks
+    if ( this === universe ) return new Yenta(options);
+    let opts  = Object.assign({},options),
+        fresh = '_' in this;
+
+    // Ensure a fully-built context
+    this._ = {
+      path : this._.path || [],
+      root : this._.root || this,
+      opts : this._.opts || opts,
+    };
+
+    // Trigger opt on fresh instance
+    if (fresh) trigger( this, 'opt' );
+  }
+
+  // Lists of hooks
+  let hooks = {};
+
+  // Trigger a hook
+  function trigger( ctx, name, data ) {
+    if (!(name in hooks)) return;
+    let queue = hooks[name].slice();
+    (function next(data) {
+      let fn = queue.shift();
+      if (!fn) return;
+      fn.call( ctx, data, next );
+    })(data);
+  }
+
+  // Register an action to a hook
+  Yenta.on = function( name, handler ) {
+    if ('function' !== typeof handler ) return Yenta;
+    if ('string' !== typeof name ) return Yenta;
+    if (!handler) return Yenta;
+    if (!name) return Yenta;
+    if (!(name in hooks)) hooks[name] = [];
+    hooks[name].push(handler);
+    return Yenta;
+  }
+
+  // Be somewhat compatible with gunjs
+  Yenta.chain = Yenta.prototype;
+
   // Return what we've built
   return Yenta;
 });
